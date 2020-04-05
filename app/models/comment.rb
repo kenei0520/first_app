cb9b1223737c5924d422a25a06ba7c9a8c3dfd62
vagrant <vagrant@localhost.localdomain>
2020-04-05 15:31:00 +0900

    first commit

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+
+  belongs_to :board
+
+
+     validates :board, presence: true
+     validates :name, presence: true
+     validates :content, presence: true, length: { in: 1..1000 }
+
+end
